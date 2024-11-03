53dff28e4962db3f63c381ec82fc39e45ede5a50
Heeku Yoon <heegu0311@gmail.com>
2024-11-03 16:57:00 +0900

    config: root path of the project

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['piggyme.s3.ap-northeast-2.amazonaws.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

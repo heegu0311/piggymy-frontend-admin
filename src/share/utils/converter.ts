3edfeab4068f796d2780c11a763cd1067b864997
Heeku Yoon <heegu0311@gmail.com>
2024-11-26 00:54:55 +0900

    config: debug configure for next full stack debug

---
+++ src/share/utils/converter.ts
@@ -0,0 +1,17 @@
+export function formDataToObject(formData: FormData): Record<string, any> {
+  const obj: Record<string, any> = {};
+
+  formData.forEach((value, key) => {
+    if (obj[key]) {
+      if (Array.isArray(obj[key])) {
+        obj[key].push(value);
+      } else {
+        obj[key] = [obj[key], value];
+      }
+    } else {
+      obj[key] = value;
+    }
+  });
+
+  return obj;
+}
